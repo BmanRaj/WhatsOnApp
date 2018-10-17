c1e42e4b08df412633007f5e9ebc6642a26f4316
vivle123 <42932703+vivle123@users.noreply.github.com>
2018-10-17 21:00:11 +1100

    implemented images for event cards

--- app/src/main/java/xyz/whatson/android/adapter/EventAdapter.java
+++ app/src/main/java/xyz/whatson/android/adapter/EventAdapter.java
@@ -1,13 +1,27 @@
 package xyz.whatson.android.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -18,6 +32,9 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
 
     private Context mContext;
     private List<Event> eventList;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    String downloadUrl;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title, startDate;
@@ -27,6 +44,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
             super(view);
             title = (TextView) view.findViewById(R.id.title);
             startDate = (TextView) view.findViewById(R.id.date);
+            image = (ImageView) view.findViewById(R.id.image);
         }
     }
 
@@ -48,6 +66,59 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
         Event event = eventList.get(position);
         holder.title.setText(event.getTitle());
         holder.startDate.setText(new SimpleDateFormat("d\nMMM").format(event.getEventDate()));
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        StorageReference ref = storageReference.child("images/" + event.getImageURL());
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                downloadUrl = uri.toString();
+                holder.image.setTag(downloadUrl);
+                new DownloadImageTask().execute(holder.image);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Log.d("FailureURL", event.getTitle());
+                holder.image.setImageResource(R.drawable.blank);
+            }
+        });
+    }
+    public class DownloadImageTask extends AsyncTask<ImageView, Void, Bitmap> {
+
+        ImageView imageView = null;
+
+        @Override
+        protected Bitmap doInBackground(ImageView... imageViews) {
+            this.imageView = imageViews[0];
+            return download_Image((String)imageView.getTag());
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+
+        private Bitmap download_Image(String src) {
+            try {
+                URL url = new URL(src);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                //myBitmap.createScaledBitmap(myBitmap, 20, 20, false);
+                Log.d("urlsrc = ", src);
+                // Bitmap myBitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d("getbitmapfailed", "getBitmapFromURL: failed");
+                return null;
+            }
+        }
     }
 
     @Override
