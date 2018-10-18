95e0958ea650afc2e5deabb11bcec98ce47aed6a
vivle123 <42932703+vivle123@users.noreply.github.com>
2018-10-18 19:54:52 +1100

    settings screen now allows user to set their category preferences

--- app/src/main/java/xyz/whatson/android/activities/settings/SettingsActivity.java
+++ app/src/main/java/xyz/whatson/android/activities/settings/SettingsActivity.java
@@ -1,38 +1,217 @@
 package xyz.whatson.android.activities.settings;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.lang.ref.Reference;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import xyz.whatson.android.R;
+import xyz.whatson.android.model.Event;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setupActionBar();
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-
-
     }
+
+
 
     public static class SettingsFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
+            CheckBoxPreference checkBoxArt, checkBoxCulture, checkBoxSports, checkBoxMusic, checkBoxTech, checkBoxScience, checkBoxRecreation, checkBoxEducation;
+            boolean artPref, culturePref, sportsPref, musicPref, techPref, sciencePref, recreationPref, educationPref;
+
+
+            FirebaseDatabase database;
+            DatabaseReference databaseReference;
+            FirebaseUser user;
+            String userID;
+
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
+            checkBoxTech = (CheckBoxPreference) findPreference("techKey");
+            checkBoxCulture = (CheckBoxPreference)findPreference("cultureKey");
+            checkBoxEducation = (CheckBoxPreference)findPreference("educationKey");
+            checkBoxMusic = (CheckBoxPreference)findPreference("musicKey");
+            checkBoxRecreation = (CheckBoxPreference)findPreference("recreationKey");
+            checkBoxScience = (CheckBoxPreference)findPreference("scienceKey");
+            checkBoxSports = (CheckBoxPreference)getPreferenceManager().findPreference("sportsKey");
+            checkBoxArt = (CheckBoxPreference)findPreference("artKey");
+
+            database = FirebaseDatabase.getInstance();
+            databaseReference = database.getReference();
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            userID = user.getUid();
+            final DatabaseReference ref = databaseReference.child("Users").child(userID);
+
+
+            //SETTING LISTENERS TO UPDATE EACH PREFERENCE IF TICKED
+            //adapted from https://stackoverflow.com/questions/28721192/android-one-clicklistener-for-many-checkboxpreference
+
+            Preference.OnPreferenceClickListener sportsListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("sportsKey");
+                    if(pref.isChecked()) {
+                        ref.child("sports").setValue("true");
+                    } else {
+                        ref.child("sports").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxSports.setOnPreferenceClickListener(sportsListener);
+
+
+            Preference.OnPreferenceClickListener artListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("artKey");
+                    if(pref.isChecked()) {
+                        ref.child("art").setValue("true");
+                    } else {
+                        ref.child("art").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxArt.setOnPreferenceClickListener(artListener);
+
+            Preference.OnPreferenceClickListener cultureListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("cultureKey");
+                    if(pref.isChecked()) {
+                        ref.child("culture").setValue("true");
+                    } else {
+                        ref.child("culture").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxCulture.setOnPreferenceClickListener(cultureListener);
+
+
+            Preference.OnPreferenceClickListener educationListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("educationKey");
+                    if(pref.isChecked()) {
+                        ref.child("education").setValue("true");
+                    } else {
+                        ref.child("education").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxEducation.setOnPreferenceClickListener(educationListener);
+
+            Preference.OnPreferenceClickListener musicListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("musicKey");
+                    if(pref.isChecked()) {
+                        ref.child("music").setValue("true");
+                    } else {
+                        ref.child("music").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxMusic.setOnPreferenceClickListener(musicListener);
+
+            Preference.OnPreferenceClickListener recreationListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("recreationKey");
+                    if(pref.isChecked()) {
+                        ref.child("recreation").setValue("true");
+                    } else {
+                        ref.child("recreation").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxRecreation.setOnPreferenceClickListener(recreationListener);
+
+            Preference.OnPreferenceClickListener scienceListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("scienceKey");
+                    if(pref.isChecked()) {
+                        ref.child("science").setValue("true");
+                    } else {
+                        ref.child("science").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxScience.setOnPreferenceClickListener(scienceListener);
+
+            Preference.OnPreferenceClickListener techListener = new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    CheckBoxPreference pref = (CheckBoxPreference) findPreference("techKey");
+                    if(pref.isChecked()) {
+                        ref.child("tech").setValue("true");
+                    } else {
+                        ref.child("tech").setValue("false");
+                    }
+
+                    return false;
+                }
+            };
+            checkBoxTech.setOnPreferenceClickListener(techListener);
+
+
         }
     }
 
